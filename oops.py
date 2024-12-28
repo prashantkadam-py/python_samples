b7f237245660602a2cf9f78f07950ec36bda60f7
prashantkadam-py <prashantkadam1912@gmail.com>
2024-12-28 21:01:38 +0530

    added class methods, static methods and instance methods sample

--- oops.py
+++ oops.py
@@ -11,7 +11,7 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = f"{self.first}.(self.last)@company.com"
+        self.email = f"{self.first}.{self.last}@company.com"
         Employee.no_of_employees += 1 # referring class variable
 
     def fullname(self):
@@ -23,6 +23,30 @@ class Employee:
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount) #class variable can be accessed using self or by specifying class name self.raise_amount / Employee.raise_amount
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+
+    @classmethod
+    def from_string(cls, emp_str):
+        """
+        Using a classmethod like constructor to create an Employee instance
+        by parsing formatted string and avoiding rewriting / for-loop to construct these objects 
+        """
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
+
+    @staticmethod
+    def isworkday(day):
+        """
+        static method it logically belongs to the class 
+        but it does not uses any class or instance reference
+        """
+        if day.weekday() in (5, 6):
+            return False
+        return True
 
 
 if __name__ == "__main__":
@@ -45,3 +69,15 @@ if __name__ == "__main__":
     print(f"Employee class name space : {Employee.__dict__} ")
     print(f"Employee instance emp_1 name space : {emp_1.__dict__} ")
     
+    Employee.set_raise_amount(1.6) #calling class method to set class variable
+    emp_1.set_raise_amount(1.65)   #calling class method using instance it sets class variable 
+    print(Employee.raise_amount)
+    print(emp_2.raise_amount)
+    print(emp_1.raise_amount)
+
+    new_emp_1 = Employee.from_string("prashant-kadam-60000") # Using classmethod as constructor and eliminating split operation outside of class
+    print(new_emp_1.email)
+    print(new_emp_1.pay)
+    import datetime
+    my_date = datetime.datetime.now()
+    print(f"Is workday : {Employee.isworkday(my_date)}")

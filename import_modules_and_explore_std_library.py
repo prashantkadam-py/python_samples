d66a694274394dc2a0f3e6a5859127385bd87b08
prashantkadam-py <prashantkadam1912@gmail.com>
2025-05-28 21:20:07 +0530

    added import modules

---
+++ import_modules_and_explore_std_library.py
@@ -0,0 +1,14 @@
+"""
+Reference 
+Corey Schafer 
+
+https://www.youtube.com/watch?v=CqvZ3vGoGs0&list=PL-osiE80TeTskrapNbzXhwoFUiLCjGgY7&index=9
+videos : 9
+"""
+
+import sys
+sys.path.append("../my_module") #Adding different directory in sys path to use this directory as module
+import my_test_module as mtm
+
+print(mtm.testing)
+
